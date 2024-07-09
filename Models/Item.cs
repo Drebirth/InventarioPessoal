9a9985f8572855a11d23219b524d0caeaeaaa465
drebirth <danielll98@outlook.com.br>
2024-07-08 23:42:50 -0300

    primeiro commit, criação da classe Item

---
+++ Models/Item.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet.Models
+{
+    public class Item
+    {   
+        private string _nome;
+        private string _descricao;
+        private int _quantidade;
+
+        public Item(string nome, string descricao, int quantidade)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Quantidade = quantidade;
+        }
+        public int Id { get; set;}
+        public string Nome 
+        { 
+            get{ return _nome; }
+            set{ _nome = value; }
+        }
+
+        public string Descricao 
+        { 
+            get => _descricao; 
+            set => _descricao = value;
+        }
+        public int Quantidade 
+        { 
+            get => _quantidade; set => _quantidade = value; 
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Projeto MVC pessoal, um sistema que funcionaria como armazém pessoal, um inventario pessoal utilizando mongodb
+177.37.172.226
+estudos
+estudos
+
+banco:
+mongodb+srv://estudos:<password>@cluster0.sa0odii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+Classe: Usuario
+atributos: Id, Usuario, Senha
+
+Classe: Item
+atributos: Id, Nome, Descricao, Quantidade
+
+
+
